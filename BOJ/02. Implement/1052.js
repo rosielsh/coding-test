3add3a15dfbe84310e995bad8d4a920a3c045346
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-09-04 00:03:14 +0900

    implement-silver-물병(해결중)

---
+++ BOJ/02. Implement/1052.js
@@ -0,0 +1,11 @@
+// 문제 : 물병
+
+// 입력 : N(지민이가 가진 물병 수), K(지민이가 옮길 수 있는 물병 수)
+// 출력 : 상점에서 사야하는 물병의 최소 값
+
+// 과정
+// 1. 
+
+const input = require('fs').readFileSync('input.txt').toString().split(' ');
+const [N, K] = input;
+console.log(N, K);
