83b32e9460249cd693a7652b9db1886281c479e8
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-10-09 23:28:28 +0900

    체스판 다시 칠하기(수정)

--- BOJ_class/BOJ_class2/1018.js
+++ BOJ_class/BOJ_class2/1018.js
@@ -1,6 +1,7 @@
 // 문제 : 체스판 다시 칠하기
 // 반복문으로 하다가 오래 걸림
 // 체스판을 미리 생성한다면 쉽게 해결되는 문제
+// 알고리즘 : 브루트 포스
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = require('fs').readFileSync(filePath).toString().trim().split('\r\n');
 const [N, M] = input.shift().split(' ');
@@ -31,30 +32,30 @@ const white = [
     "WBWBWBWB",
   ];
 
-const check = (i, j) => {
+const check = (x, y) => {
     let countA = 0;
     let countB = 0;
 
-    for(let a = 0; a<8; a++) {
-        for(let b = 0; b<8; b++) {
+    for(let i = y; i<y+8; i++) { // 행
+        for(let j = x; j<x+8; j++) { // 열
             
-            if(white[a][b] !== board[i+a][j+b]){
+            if(white[i][j] !== board[i-y][j-x]){
                 countA++;
             }
 
-            if(black[a][b] !== board[i+a][j+b]){
+            if(black[i][j] !== board[i-y][j-x]){
                 countB++;
             }
         }
     }
-    return countA > countB ? countA : countB;
+    return countA > countB ? countB : countA;
 }
 
 function solution() {
     let cntList = [];
     for(let i = 0; i+8 <= N; i++) {
         for(let j = 0; j+8 <= M; j++) {
-            cntList.push(check(i, j));
+            cntList.push(check(j, i));
         }
     }
 
