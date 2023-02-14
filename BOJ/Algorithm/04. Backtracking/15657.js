666b19c1fd5a3ee765663eee9c08cffe0b6d39d0
suhwa-lee <tnghk9611@kumoh.ac.kr>
2023-02-14 15:56:05 +0900

    N과 M

---
+++ BOJ/Algorithm/04. Backtracking/15657.js
@@ -0,0 +1,22 @@
+// N과 M (8)
+
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+const [N, M] = input[0].split(' ').map(Number);
+const arr = input[1].split(' ').map(Number).sort((a, b) => a-b);
+const visited = Array.from({length: M}, ()=>0);
+
+function solution(len, idx) {
+  if(len === M) {
+    console.log(visited.join(' '));
+    return;
+  }
+
+  for(let i=idx; i<N; i++) {
+    visited[len] = arr[i];
+    solution(len+1, i);
+    visited[len] = 0;
+  }
+}
+
+solution(0, 0);
