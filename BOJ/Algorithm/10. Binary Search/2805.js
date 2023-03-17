391287b9018856a9d28773a0539e424cc55707e3
suhwa-lee <tnghk9611@kumoh.ac.kr>
2023-03-17 15:14:19 +0900

    BOJ_나무 자르기

--- BOJ/Algorithm/10. Binary Search/2805.js
+++ BOJ/Algorithm/10. Binary Search/2805.js
@@ -1,28 +1,36 @@
-// 나무 자르기
-
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
-const [N, M] = input[0].split(' ').map(Number);
-const tree = input[1].split(' ').map(Number).sort((a, b)=>a-b);
+[NM, tree] = require('fs').readFileSync(filePath).toString().trim().split('\n');
+[N, M] = NM.split(' ').map(Number);
+tree = tree.split(' ').map(Number);
+
+// 나무 자르기
+// 상근이가 H 높이 만큼 나무를 잘라 최소 M 만큼의 나무를 가져가야 함
+
+// 최소 나무 높이 : 0
+// 최대 나무 높이 : 나무 중 최댓값 
 
 function solution() {
   let left = 0;
-  let right = tree[N-1];
+  let right = Math.max(...tree);
   let mid;
-  let sum;
-  while(left <= right) {
-    sum = 0;
-    mid = parseInt((left+right)/2);
-    
-    for(let i=0; i<N; i++) {
-      sum += tree[i]>mid ? tree[i]-mid:0;
-    }
 
-    if(sum >= M) {
-      left = mid+1;
-    } else {
-      right = mid-1;
-    }
+  while(left <= right) {
+	mid = parseInt((left + right)/2);
+
+	let getTree = 0;
+	for(let i=0; i<N; i++) {
+		if(tree[i] - mid > 0) {
+			getTree += (tree[i] - mid);
+		}
+	}
+
+	if(getTree === M) {
+		return mid;
+	} else if(getTree > M) {
+		left = mid + 1;
+	} else {
+		right = mid - 1;
+	}
   }
 
   return right;
