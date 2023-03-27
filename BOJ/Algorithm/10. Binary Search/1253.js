d22036dc766b2eab4cbc5d319a4e5946ec461370
suhwa-lee <tnghk9611@kumoh.ac.kr>
2023-03-27 19:30:12 +0900

    BOJ_좋다

---
+++ BOJ/Algorithm/10. Binary Search/1253.js
@@ -0,0 +1,46 @@
+// 좋다
+
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+[N, numbers] = require("fs").readFileSync(filePath).toString().trim().split("\n");
+N = +N;
+numbers = numbers
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
+
+function binarySearch(index, currentNum) {
+  let left = 0;
+  let right = N - 1;
+  let sum;
+
+  if (left === index) left++;
+  if (right === index) right--;
+
+  while (left < right) {
+    sum = numbers[left] + numbers[right];
+    if (sum === currentNum) {
+      return true;
+    } else if (sum < currentNum) {
+      left++;
+      if (left === index) left++;
+    } else {
+      right--;
+      if (right === index) right--;
+    }
+  }
+
+  return false;
+}
+
+function solution() {
+  let answer = 0;
+
+  if (N <= 2) return 0;
+
+  for (let i = 0; i < N; i++) {
+    if (binarySearch(i, numbers[i])) answer++;
+  }
+  return answer;
+}
+
+console.log(solution());
