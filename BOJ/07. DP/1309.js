de4462ef4e2e2c018c177c8d231ea61c846381cf
suhwa-lee <tnghk9611@kumoh.ac.kr>
2023-04-22 20:38:23 +0900

    modify 1309

--- BOJ/07. DP/1309.js
+++ BOJ/07. DP/1309.js
@@ -5,15 +5,16 @@ const N = require("fs").readFileSync(filePath).toString().trim().split("\n").map
 
 function solution() {
   let answer;
-  const dp = Array.from({ length: N + 1 }, () => 0);
-  dp[1] = 3;
-  dp[2] = 7;
+  const dp = Array.from({ length: N + 1 }, () => Array(3).fill(0));
+  dp[1] = [1, 1, 1];
 
-  for (let i = 3; i <= N; i++) {
-    dp[i] = (dp[i - 2] * 3 + (dp[i - 1] - dp[i - 2]) * 2) % 9901;
+  for (let i = 2; i <= N; i++) {
+    dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % 9901;
+    dp[i][1] = (dp[i - 1][0] + dp[i - 1][1]) % 9901;
+    dp[i][2] = (dp[i - 1][0] + dp[i - 1][2]) % 9901;
   }
 
-  answer = dp[N];
+  answer = dp[N].reduce((acc, cur) => acc + cur, 0);
   return answer;
 }
 
