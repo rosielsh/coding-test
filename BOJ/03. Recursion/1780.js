ea38bb66d6aff647b656d6729c58a214a3f95a6b
suhwa-lee <tnghk9611@kumoh.ac.kr>
2023-07-24 19:45:51 +0900

    BOJ_종이의 개수

--- BOJ/03. Recursion/1780.js
+++ BOJ/03. Recursion/1780.js
@@ -1,44 +1,56 @@
 // 종이의 개수
 
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-[N, ...paper] = require("fs").readFileSync(filePath).toString().trim().split("\n");
+let [N, ...paper] = require("fs").readFileSync(filePath).toString().trim().split("\n");
 N = +N;
 paper = paper.map((x) => x.split(" ").map(Number));
 
-let minus = 0;
-let zero = 0;
-let plus = 0;
-
-function solution(startY, startX, len) {
-  if (len === 1) {
-    if (paper[startY][startX] === -1) minus += 1;
-    else if (paper[startY][startX] === 0) zero += 1;
-    else plus += 1;
-    return;
-  }
-
-  let std = paper[startY][startX];
-  let flag = true;
-  for (let i = startY; i <= startY + len - 1; i++) {
-    for (let j = startX; j <= startX + len - 1; j++) {
-      if (paper[i][j] !== std) flag = false;
+function checkIsSame(x, y, size) {
+  let std = paper[x][y];
+  for (let i = x; i < x + size; i++) {
+    for (let j = y; j < y + size; j++) {
+      if (paper[i][j] !== std) return false;
     }
   }
 
-  if (flag) {
-    if (std === -1) minus += 1;
-    else if (std === 0) zero += 1;
-    else plus += 1;
+  return true;
+}
+
+const cnt = Array.from({ length: 3 }, () => 0);
+
+function search(r, c, size) {
+  if (size === 0) {
     return;
   }
 
-  let nextLen = parseInt(len / 3);
+  if (checkIsSame(r, c, size)) {
+    if (paper[r][c] === -1) {
+      cnt[0]++;
+    } else if (paper[r][c] === 0) {
+      cnt[1]++;
+    } else {
+      cnt[2]++;
+    }
+    return;
+  }
+
+  const nextSize = parseInt(size / 3);
+
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
-      solution(startY + nextLen * i, startX + nextLen * j, nextLen);
+      const nr = r + nextSize * i;
+      const nc = c + nextSize * j;
+
+      search(nr, nc, nextSize);
     }
   }
 }
 
-solution(0, 0, N);
-console.log(`${minus}\n${zero}\n${plus}`);
+function solution() {
+  let answer;
+  search(0, 0, N);
+  answer = cnt.join("\n");
+  return answer;
+}
+
+console.log(solution());
