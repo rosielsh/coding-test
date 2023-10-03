4162ebb4ac872d4a564cb2d757fbf6c78d8afd58
suhwa-lee <tnghk9611@kumoh.ac.kr>
2023-10-03 23:13:56 +0900

    BOJ_벽 부수고 이동하기

--- Javascript/BOJ/2206.js
+++ Javascript/BOJ/2206.js
@@ -1,50 +1,54 @@
-// 벽 부수고 이동하기
-
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-[NM, ...map] = require("fs").readFileSync(filePath).toString().trim().split("\n");
-[N, M] = NM.split(" ").map(Number);
-map = map.map((x) => x.replace("\r", "").split("").map(Number));
+const input = require("fs").readFileSync(filePath).toString().replace(/\r/g, "").trim().split("\n");
 
-const dy = [0, 0, -1, 1];
+const [N, M] = input.shift().split(" ").map(Number);
+const map = input.map((x) => x.split("").map(Number));
+
 const dx = [-1, 1, 0, 0];
+const dy = [0, 0, -1, 1];
 
-// visited[a][b][0/1] => 0:벽을 부수지 않고 방문, 1:벽을 부수고 방문
-// ex) visited[a][b][1] = (a, b)까지 벽을 부쉈을 때 이동한 최단거리 저장
-const visited = Array.from({ length: N }, () => Array.from({ length: M }, () => Array(2).fill(0)));
+function bfs() {
+  const queue = [[0, 0, 0]];
+  const visited = Array.from({ length: N }, () =>
+    Array.from({ length: M }, () => Array(2).fill(0))
+  );
 
-function bfs(startY, startX) {
-  // 인자 : 시작 행, 시작 열, 벽을 깼는지 여부
-  const queue = [[startY, startX, 0]];
-  visited[startY][startX][0] = 1;
+  visited[0][0][0] = 1;
+
+  // visited[a][b][0/1] => 0:벽을 부수지 않고 방문, 1:벽을 부수고 방문
+  // ex) visited[a][b][1] = (a, b)까지 벽을 부쉈을 때 이동한 최단거리 저장
   let idx = 0;
-
   while (queue.length !== idx) {
-    [curY, curX, isBreak] = queue[idx];
+    const [x, y, isBreak] = queue[idx];
 
-    if (curY === N - 1 && curX === M - 1) {
-      return visited[curY][curX][isBreak];
+    if (x === N - 1 && y === M - 1) {
+      // 현재 상태에서의 최단 경로 반환
+      return visited[N - 1][M - 1][isBreak];
     }
 
     for (let i = 0; i < 4; i++) {
-      ny = curY + dy[i];
-      nx = curX + dx[i];
+      const nx = x + dx[i];
+      const ny = y + dy[i];
 
-      if (ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
+      if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
 
-      // 이동할 위치가 벽이 아니고 방문하지 않은 경우
-      if (!map[ny][nx] && !visited[ny][nx][isBreak]) {
-        visited[ny][nx][isBreak] = visited[curY][curX][isBreak] + 1;
-        queue.push([ny, nx, isBreak]);
+      // 간 곳이 벽인 경우
+      if (map[nx][ny]) {
+        if (isBreak) continue;
+        visited[nx][ny][1] = visited[x][y][0] + 1;
+        queue.push([nx, ny, 1]);
       }
-      // 이동할 위치가 벽이고, 아직 부수지 않았다면
-      else if (map[ny][nx] && !isBreak) {
-        visited[ny][nx][1] = visited[curY][curX][0] + 1;
-        queue.push([ny, nx, 1]);
+      // 간 곳이 벽이 아닌 경우
+      else {
+        if (visited[nx][ny][isBreak]) continue;
+        visited[nx][ny][isBreak] = visited[x][y][isBreak] + 1;
+        queue.push([nx, ny, isBreak]);
       }
     }
     idx++;
   }
+
   return -1;
 }
 
-console.log(bfs(0, 0));
+console.log(bfs());
