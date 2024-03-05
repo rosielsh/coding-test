f96864e6ad4e3455700fb6b52fd731d73ddea3db
이수화 <tnghk9611@naver.com>
2024-03-05 12:38:09 +0900

    pq 수정

--- Javascript/Basic Code/dataStructure/PriorityQueue.js
+++ Javascript/Basic Code/dataStructure/PriorityQueue.js
@@ -14,12 +14,16 @@ class PriorityQueue {
         this.heapifyUp();
     }
 
+    // 최소힙으로 정렬하는 함수
     heapifyUp() {
+        // 현재 원소가 들어간 인덱스
         let idx = this.heap.length - 1;
 
         while (idx > 0) {
+            // 부모 인덱스 => 3번 자식의 부모는 1번
             const pIdx = Math.floor((idx - 1) / 2);
 
+            // 자식이 더 크면 종료
             if (this.heap[idx] >= this.heap[pIdx]) break;
 
             this.swap(idx, pIdx);
@@ -35,29 +39,31 @@ class PriorityQueue {
         }
 
         const min = this.heap[0];
-        this.heap[0] = this.heap.pop();
+        this.heap[0] = this.heap.pop(); // 제일 뒤에 있는 원소를 제일 앞으로
         this.heapifyDown(0);
 
         return min;
     }
 
     heapifyDown(idx) {
-        let leftChildIdx = 2 * idx + 1;
-        let rightChildIdx = 2 * idx + 2;
+        const leftChildIdx = idx * 2 + 1;
+        const rightChildIdx = idx * 2 + 2;
 
-        let smallIdx = idx;
+        // 결과적으로 젤 작은 인덱스
+        let minIdx = idx;
 
-        if (leftChildIdx < this.heap.length && this.heap[leftChildIdx] < this.heap[smallIdx]) {
-            smallIdx = leftChildIdx;
+        if (leftChildIdx < this.heap.length && this.heap[leftChildIdx] < this.heap[minIdx]) {
+            minIdx = leftChildIdx;
         }
 
-        if (rightChildIdx < this.heap.length && this.heap[rightChildIdx] < this.heap[smallIdx]) {
-            smallIdx = rightChildIdx;
+        if (rightChildIdx < this.heap.length && this.heap[rightChildIdx] < this.heap[minIdx]) {
+            minIdx = rightChildIdx;
         }
 
-        if (smallIdx === idx) return;
+        // 내가 젤 작음
+        if (minIdx === idx) return;
 
-        this.swap(idx, smallIdx);
-        this.heapifyDown(smallIdx);
+        this.swap(minIdx, idx);
+        this.heapifyDown(minIdx);
     }
 }
