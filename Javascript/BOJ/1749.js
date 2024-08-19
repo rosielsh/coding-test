74557bf266195a715a8e06231ec657ae52983ebd
suhwa <tnghk9611@naver.com>
2024-08-19 12:03:04 +0900

    BOJ_점수따먹기

--- Javascript/BOJ/1749.js
+++ Javascript/BOJ/1749.js
@@ -1,6 +1,5 @@
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
-
 const [N, M] = input.shift().split(" ").map(Number);
 const arr = input.map((x) => x.split(" ").map(Number));
 
@@ -15,16 +14,14 @@ for (let i = 1; i <= N; i++) {
 
 let answer = Number.MIN_SAFE_INTEGER;
 
-for (let i = 1; i <= N; i++) {
-    for (let j = 1; j <= M; j++) {
-        for (let k = i; k <= N; k++) {
-            for (let l = j; l <= M; l++) {
-                const sum =
-                    prefix[k][l] - prefix[i - 1][l] - prefix[k][j - 1] + prefix[i - 1][j - 1];
-
-                if (answer < sum) {
-                    answer = sum;
-                }
+for (let a = 1; a <= N; a++) {
+    for (let b = 1; b <= M; b++) {
+        for (let c = a; c <= N; c++) {
+            for (let d = b; d <= M; d++) {
+                answer = Math.max(
+                    answer,
+                    prefix[c][d] - prefix[c][b - 1] - prefix[a - 1][d] + prefix[a - 1][b - 1]
+                );
             }
         }
     }
