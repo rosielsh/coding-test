2ff19fd72f5a0feb1205c8071767b2502a501d71
suhwa <tnghk9611@naver.com>
2024-09-12 20:35:54 +0900

    BOJ_트리의 지름

--- Javascript/BOJ/1967.js
+++ Javascript/BOJ/1967.js
@@ -1,45 +1,39 @@
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
+const n = input.shift();
+const edges = input.map((x) => x.split(" ").map(Number));
 
-// 트리의 지름 알고리즘
-// 임의의 정점에서 가장 먼 노드를 찾고, 그 노드에서 가장 먼 노드 찾기
-
-const n = +input.shift();
 const graph = Array.from({ length: n + 1 }, () => []);
+let visited;
 
-for (let i = 0; i < n - 1; i++) {
-    const [p, c, w] = input[i].split(" ").map(Number);
-    graph[p].push([c, w]);
-    graph[c].push([p, w]);
+for (let [from, to, weight] of edges) {
+    graph[from].push([to, weight]);
+    graph[to].push([from, weight]);
 }
 
-const bfs = (idx) => {
-    const queue = [[idx, 0]];
-    const visited = Array.from({ length: n + 1 }, () => false);
-    visited[idx] = true;
+let maxNode = 0;
+let maxVal = 0;
 
-    // 최대 노드값, 그때의 지름
-    let max = [0, 0];
-
-    while (queue.length) {
-        const [cIdx, sum] = queue.shift();
-
-        for (let [i, w] of graph[cIdx]) {
-            if (visited[i]) continue;
-            visited[i] = true;
-
-            // 최댓값 갱신
-            if (max[1] < sum + w) {
-                max[0] = i;
-                max[1] = sum + w;
-            }
-
-            queue.push([i, sum + w]);
-        }
+const dfs = (x, sum) => {
+    if (maxVal < sum) {
+        maxVal = sum;
+        maxNode = x;
     }
 
-    return max;
+    for (let [next, nweight] of graph[x]) {
+        if (visited[next]) continue;
+        visited[next] = true;
+        dfs(next, sum + nweight);
+    }
 };
 
-const fNode = bfs(1);
-console.log(bfs(fNode[0])[1]);
+visited = Array.from({ length: n + 1 }, () => false);
+visited[1] = true;
+dfs(1, 0);
+
+visited = Array.from({ length: n + 1 }, () => false);
+visited[maxNode] = true;
+maxVal = 0;
+dfs(maxNode, 0);
+
+console.log(maxVal);
