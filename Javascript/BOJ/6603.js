e68ab406324cf55028462f554815a7760012c1fa
suhwa <tnghk9611@naver.com>
2024-09-30 22:00:53 +0900

    BOJ_로또

--- Javascript/BOJ/6603.js
+++ Javascript/BOJ/6603.js
@@ -1,31 +1,25 @@
-// 로또
-
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
-let lottoSet = [];
-let N = 0;
-const lottoWin = Array.from({ length: 6 }, () => 0);
 
-function backTracking(n, idx) {
-  if (n === 6) {
-    console.log(lottoWin.join(" "));
-    return;
-  }
+const answer = [];
 
-  for (let i = idx; i < lottoSet.length; i++) {
-    lottoWin[n] = lottoSet[i];
-    backTracking(n + 1, i + 1);
-    lottoWin[n] = 0;
-  }
+const bt = (depth, idx, k, S, selected) => {
+    if (depth === 6) {
+        answer.push(selected.trim());
+        return;
+    }
+
+    for (let i = idx; i < k; i++) {
+        bt(depth + 1, i + 1, k, S, selected + `${S[i]} `);
+    }
+};
+
+let idx = 0;
+
+while (input[idx] !== "0") {
+    const [k, ...S] = input[idx++].split(" ").map(Number);
+    bt(0, 0, k, S, "");
+    answer.push("");
 }
 
-function solution() {
-  while (true) {
-    [N, ...lottoSet] = input.shift().split(" ").map(Number);
-    if (N === 0) break;
-    backTracking(0, 0);
-    console.log("");
-  }
-}
-
-solution();
+console.log(answer.join("\n").trim());
