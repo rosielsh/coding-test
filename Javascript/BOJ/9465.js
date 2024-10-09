6c262617cf9bf42c8cbbac3def3b31103c166dd2
suhwa <tnghk9611@naver.com>
2024-10-09 13:46:46 +0900

    BOJ_스티커

--- Javascript/BOJ/9465.js
+++ Javascript/BOJ/9465.js
@@ -1,28 +1,26 @@
-// 스티커
-
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-[T, ...stickers] = require("fs").readFileSync(filePath).toString().trim().split("\n");
+const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
 
-function solution() {
-  let answer = [];
-  for (let i = 0; i < T; i++) {
-    let cnt = +stickers.shift();
-    const sticker = stickers.splice(0, 2).map((x) => x.split(" ").map(Number));
+const answer = [];
+const T = +input[0];
 
-    const dp = Array.from({ length: cnt + 1 }, () => Array(2).fill(0));
+let idx = 1;
+for (let t = 0; t < T; t++) {
+    const n = +input[idx++];
+    const sticker = input.slice(idx, idx + 2).map((x) => x.split(" ").map(Number));
+    idx += 2;
+
+    const dp = Array.from({ length: n + 1 }, () => Array(2).fill(0));
 
     dp[1][0] = sticker[0][0];
     dp[1][1] = sticker[1][0];
 
-    for (let i = 2; i <= cnt; i++) {
-      dp[i][0] = Math.max(dp[i - 1][1], dp[i - 2][1]) + sticker[0][i - 1];
-      dp[i][1] = Math.max(dp[i - 1][0], dp[i - 2][0]) + sticker[1][i - 1];
+    for (let i = 2; i <= n; i++) {
+        dp[i][0] = Math.max(dp[i - 2][0], dp[i - 2][1], dp[i - 1][1]) + sticker[0][i - 1];
+        dp[i][1] = Math.max(dp[i - 2][0], dp[i - 2][1], dp[i - 1][0]) + sticker[1][i - 1];
     }
 
-    answer.push(Math.max(...dp.at(-1)));
-  }
-
-  return answer.join("\n");
+    answer.push(Math.max(...dp[n]));
 }
 
-console.log(solution());
+console.log(answer.join("\n"));
