b688c1eaa427294760e40ad76baaab2537ad59d7
suhwa <tnghk9611@naver.com>
2024-10-12 18:43:52 +0900

    PG_프로세스

--- Javascript/Programmers/Algorithm/프로세스.js
+++ Javascript/Programmers/Algorithm/프로세스.js
@@ -1,37 +1,26 @@
 function solution(priorities, location) {
     var answer = 0;
 
+    const N = priorities.length;
     const queue = [];
+    const run = [];
 
-    for (let i = 0; i < priorities.length; i++) {
+    for (let i = 0; i < N; i++) {
         queue.push([i, priorities[i]]);
     }
 
-    let flag = true;
-    while (flag) {
-        const fP = queue[0][1];
+    while (queue.length > 0) {
+        const [idx, priority] = queue.shift();
+        const hasBigger = queue.some((ele) => ele[1] > priority);
 
-        let hasBig = false;
-        for (let i = 1; i < queue.length; i++) {
-            if (fP < queue[i][1]) {
-                hasBig = true;
-                break;
-            }
+        if (hasBigger) {
+            queue.push([idx, priority]);
+            continue;
         }
 
-        // 더 큰게 없음
-        if (!hasBig) {
-            const [idx, val] = queue.shift();
-
-            if (idx === location) {
-                return answer + 1;
-            }
-
-            answer++;
-        } else {
-            queue.push(queue.shift());
-        }
+        run.push(idx);
     }
 
+    answer = run.indexOf(location) + 1;
     return answer;
 }
