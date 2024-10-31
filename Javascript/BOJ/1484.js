f2c4606ee1a3293d6bf35816642d91d49c3a3f52
suhwa <tnghk9611@naver.com>
2024-10-31 21:17:33 +0900

    BOJ_다이어트

---
+++ Javascript/BOJ/1484.js
@@ -0,0 +1,25 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
+const G = +input[0];
+
+const arr = Array.from({ length: G + 1 }, (_, idx) => idx);
+
+const answer = [];
+
+let left = 1;
+let right = 1;
+
+while (right < G) {
+    const sub = arr[right] ** 2 - arr[left] ** 2;
+
+    if (sub < G) {
+        right++;
+    } else if (sub > G) {
+        left++;
+    } else if (sub === G) {
+        answer.push(right);
+        right++;
+    }
+}
+
+console.log(answer.length === 0 ? -1 : answer.sort((a, b) => a - b).join("\n"));
