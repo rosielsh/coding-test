d75c0e7c5c73fb280e3e35a2412d7cd9f94bd3ad
suhwa <tnghk9611@naver.com>
2024-11-01 15:30:34 +0900

    BOJ_하늘에서 별똥별이 빗발친다

--- Javascript/BOJ/14658.js
+++ Javascript/BOJ/14658.js
@@ -1,41 +1,26 @@
-// 하늘에서 별똥별이 빗발친다
-
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
+const [N, M, L, K] = input.shift().split(" ").map(Number);
+const star = input.map((x) => x.split(" ").map(Number));
 
-[N, M, L, K] = input[0].split(" ").map(Number);
-starPos = input.slice(1).map((x) => x.split(" ").map(Number));
+let answer = 0;
 
-function setTrampoline(startX, startY) {
-  let cnt = 0;
-
-  for (let i = 0; i < K; i++) {
-    if (
-      starPos[i][0] >= startX &&
-      starPos[i][0] <= startX + L &&
-      starPos[i][1] >= startY &&
-      starPos[i][1] <= startY + L
-    )
-      cnt++;
-  }
-
-  return cnt;
-}
-
-function solution() {
-  let answer;
-
-  // 최대로 트램펄린과 겹치는 별똥별 수
-  let maxStar = Number.MIN_SAFE_INTEGER;
-
-  for (let i = 0; i < K; i++) {
+for (let i = 0; i < K; i++) {
     for (let j = 0; j < K; j++) {
-      maxStar = Math.max(setTrampoline(starPos[i][0], starPos[j][1]), maxStar);
-    }
-  }
+        const [x1, y1] = star[i];
+        const [x2, y2] = star[j];
 
-  answer = K - maxStar;
-  return answer;
+        let cnt = 0;
+
+        for (let s = 0; s < K; s++) {
+            const [x, y] = star[s];
+            if (x >= x1 && x <= x1 + L && y >= y2 && y <= y2 + L) {
+                cnt++;
+            }
+        }
+
+        answer = Math.max(answer, cnt);
+    }
 }
 
-console.log(solution());
+console.log(K - answer);
