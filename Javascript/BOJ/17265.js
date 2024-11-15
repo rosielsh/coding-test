3afdc6bfbbb626df356c080f139c43dee0119057
suhwa <tnghk9611@naver.com>
2024-11-15 15:12:02 +0900

    BOJ_나의 인생에는 수학과 함께

---
+++ Javascript/BOJ/17265.js
@@ -0,0 +1,56 @@
+// nodejs로 통과 x
+// 같은 로직으로 java로 제출하면 맞음
+
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
+
+const N = +input.shift();
+const map = input.map((x) => x.split(" "));
+
+const calc = (a, b, oper) => {
+  switch (oper) {
+    case "+": {
+      return a + b;
+    }
+    case "-": {
+      return a - b;
+    }
+    case "*": {
+      return a * b;
+    }
+  }
+};
+
+let maxValue = -Infinity;
+let minValue = Infinity;
+
+const dx = [1, 0];
+const dy = [0, 1];
+
+const dfs = (x, y, value, oper) => {
+  if (x === N - 1 && y === N - 1) {
+    maxValue = Math.max(maxValue, value);
+    minValue = Math.min(minValue, value);
+    return;
+  }
+
+  for (let i = 0; i < 2; i++) {
+    const nx = x + dx[i];
+    const ny = y + dy[i];
+
+    if (nx >= N || ny >= N) continue;
+
+    // 다음칸: 연산자
+    if ((nx + ny) % 2 === 1) {
+      dfs(nx, ny, value, map[nx][ny]);
+    }
+    // 다음칸: 숫자
+    else {
+      dfs(nx, ny, calc(value, +map[nx][ny], oper), null);
+    }
+  }
+};
+
+dfs(0, 0, +map[0][0], null);
+
+console.log(maxValue, minValue);
