c595fd23779880b91018fa7d4e886c5dee1b7cf6
suhwa <tnghk9611@naver.com>
2024-11-20 14:06:08 +0900

    BOJ_자두나무

--- Javascript/BOJ/2240.js
+++ Javascript/BOJ/2240.js
@@ -2,34 +2,24 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
 
 const [T, W] = input.shift().split(" ").map(Number);
-const list = input.map(Number);
+const tree = input.map(Number);
 
-const dp = Array.from({ length: W + 1 }, () =>
-  Array.from({ length: T + 1 }, () => Array(2).fill(0))
-);
+const dp = Array.from({ length: W + 1 }, () => Array.from({ length: T + 1 }, () => Array(2).fill(0)));
 
-for (let w = 0; w <= W; w++) {
-  for (let t = 1; t <= T; t++) {
-    if (w === 0) {
-      dp[w][t][0] = dp[w][t - 1][0] + (list[t - 1] === 1 ? 1 : 0);
-    } else {
-      dp[w][t][0] = Math.max(
-        dp[w - 1][t - 1][1] + (list[t - 1] === 1 ? 1 : 0),
-        dp[w][t - 1][0] + (list[t - 1] === 1 ? 1 : 0)
-      );
-      dp[w][t][1] = Math.max(
-        dp[w - 1][t - 1][0] + (list[t - 1] === 2 ? 1 : 0),
-        dp[w][t - 1][1] + (list[t - 1] === 2 ? 1 : 0)
-      );
+for (let i = 0; i <= W; i++) {
+  for (let j = 1; j <= T; j++) {
+    for (let k = 0; k < 2; k++) {
+      if (i === 0) {
+        dp[i][j][0] = dp[i][j - 1][0] + (tree[j - 1] === 1 ? 1 : 0);
+      } else {
+        if (k === 0) {
+          dp[i][j][k] = Math.max(dp[i][j][k], dp[i - 1][j - 1][1], dp[i][j - 1][0]) + (tree[j - 1] === 1 ? 1 : 0);
+        } else {
+          dp[i][j][k] = Math.max(dp[i][j][k], dp[i - 1][j - 1][0], dp[i][j - 1][1]) + (tree[j - 1] === 2 ? 1 : 0);
+        }
+      }
     }
   }
 }
 
-let maxValue = -1;
-for (let i = 0; i <= W; i++) {
-  for (let j = 0; j <= 1; j++) {
-    maxValue = Math.max(maxValue, dp[i][T][j]);
-  }
-}
-
-console.log(maxValue);
+console.log(Math.max(...dp[W][T]));
