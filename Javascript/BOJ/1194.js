bcc2d8c1384005aaca2f2e6afa3019935c7c3a6e
suhwa <tnghk9611@naver.com>
2024-11-26 15:36:17 +0900

    BOJ_달이 차오른다, 가자.

--- Javascript/BOJ/1194.js
+++ Javascript/BOJ/1194.js
@@ -1,70 +1,73 @@
-// 달이 차오른다, 가자.
-
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let [NM, ...miro] = require("fs").readFileSync(filePath).toString().trim().split("\n");
-const [N, M] = NM.split(" ").map(Number);
-miro = miro.map((x) => x.replace("\r", "").split(""));
+const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
+
+const [N, M] = input[0].split(" ").map(Number);
+const map = input.slice(1).map((x) => x.split(""));
+
+const visited = Array.from({ length: 64 }, () => Array.from({ length: N }, () => Array(M).fill(false)));
+
+let start = [0, 0];
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < M; j++) {
+    if (map[i][j] === "0") {
+      start[0] = i;
+      start[1] = j;
+    }
+  }
+}
+
+const keys = ["f", "e", "d", "c", "b", "a"];
+const doors = ["F", "E", "D", "C", "B", "A"];
 
 const dx = [0, 0, -1, 1];
 const dy = [-1, 1, 0, 0];
 
-function bfs(x, y) {
-  const visited = Array.from({ length: 64 }, () =>
-    Array.from({ length: N }, () => Array(M).fill(0))
-  );
-  const queue = [[x, y, 0, 0]];
-  visited[0][x][y] = 1;
+let answer = -1;
+const queue = [[...start, 0, 0]];
 
-  while (queue.length) {
-    let [cx, cy, dist, key] = queue.shift();
+visited[0][start[0]][start[1]] = true;
 
-    // 탈출
-    if (miro[cx][cy] === "1") return dist;
+while (queue.length > 0) {
+  const [x, y, cnt, key] = queue.shift();
 
-    for (let i = 0; i < 4; i++) {
-      const nx = dx[i] + cx;
-      const ny = dy[i] + cy;
-
-      if (nx < 0 || nx >= N || ny < 0 || ny >= M || miro[nx][ny] === "#" || visited[key][nx][ny])
-        continue;
-
-      // 키
-      if (miro[nx][ny].charCodeAt(0) >= 97 && miro[nx][ny].charCodeAt(0) <= 102) {
-        let nextKey = 1 << (miro[nx][ny].charCodeAt(0) - 97);
-        nextKey = key | nextKey;
-        if (visited[nextKey][nx][ny]) continue; // 다음 키를 추가한 상태에서 방문한 적이 있으면 pass
-        visited[nextKey][nx][ny] = 1;
-        queue.push([nx, ny, dist + 1, nextKey]);
-      }
-
-      // 문
-      else if (miro[nx][ny].charCodeAt(0) >= 65 && miro[nx][ny].charCodeAt(0) <= 70) {
-        const door = 1 << (miro[nx][ny].charCodeAt(0) - 65);
-        if ((door & key) > 0) {
-          visited[key][nx][ny] = 1;
-          queue.push([nx, ny, dist + 1, key]);
-        }
-      }
-
-      // .
-      else {
-        visited[key][nx][ny] = 1;
-        queue.push([nx, ny, dist + 1, key]);
-      }
-    }
+  if (map[x][y] === "1") {
+    answer = cnt;
+    break;
   }
-  return -1;
+
+  for (let i = 0; i < 4; i++) {
+    const nx = x + dx[i];
+    const ny = y + dy[i];
+
+    if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+    if (map[nx][ny] === "#" || visited[key][nx][ny]) continue;
+
+    if (keys.includes(map[nx][ny])) {
+      const val = 1 << keys.indexOf(map[nx][ny]);
+      const nextKey = key | val;
+
+      if (visited[nextKey][nx][ny]) continue;
+      visited[nextKey][nx][ny] = cnt + 1;
+
+      queue.push([nx, ny, cnt + 1, nextKey]);
+      continue;
+    }
+
+    if (doors.includes(map[nx][ny])) {
+      const door = 1 << doors.indexOf(map[nx][ny]);
+      const possibleOpen = door & key;
+
+      if (possibleOpen > 0) {
+        visited[key][nx][ny] = true;
+        queue.push([nx, ny, cnt + 1, key]);
+      }
+
+      continue;
+    }
+
+    visited[key][nx][ny] = 1;
+    queue.push([nx, ny, cnt + 1, key]);
+  }
 }
 
-function solution() {
-  let answer;
-  miro.forEach((row, i) => {
-    row.forEach((col, j) => {
-      if (col === "0") answer = bfs(i, j);
-    });
-  });
-
-  return answer;
-}
-
-console.log(solution());
+console.log(answer);
