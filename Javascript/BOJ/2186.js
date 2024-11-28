5a6cac1024a89a2d58eeeb48f3aa60e90c91bc81
suhwa <tnghk9611@naver.com>
2024-11-28 16:41:36 +0900

    BOJ_문자판

--- Javascript/BOJ/2186.js
+++ Javascript/BOJ/2186.js
@@ -1,44 +1,48 @@
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
-const [N, M, K] = input.shift().split(" ").map(Number);
-const words = input.splice(0, N).map((x) => x.split(""));
-const findWord = input[0];
 
-const dx = [-1, 1, 0, 0];
-const dy = [0, 0, -1, 1];
+const [N, M, K] = input[0].split(" ").map(Number);
+const map = input.slice(1, N + 1).map((x) => x.split(""));
+const alpha = input.slice(N + 1)[0].split("");
 
-const visited = Array.from({ length: N }, () =>
-    Array.from({ length: M }, () => Array(K + 1).fill(-1))
-);
+let dp = Array.from({ length: alpha.length + 1 }, () => Array.from({ length: N }, () => Array(M).fill(-1))); // 각 지점에서 만들 수 있는 경로의 개수 저장
 
-const dfs = (depth, x, y) => {
-    if (depth === findWord.length) return (visited[x][y][depth] = 1);
-    if (visited[x][y][depth] >= 0) return visited[x][y][depth];
+const dx = [0, 0, -1, 1];
+const dy = [-1, 1, 0, 0];
 
-    visited[x][y][depth] = 0;
+const dfs = (x, y, idx) => {
+  if (dp[idx][x][y] !== -1) {
+    return dp[idx][x][y];
+  }
 
-    for (let i = 0; i < 4; i++) {
-        for (let k = 1; k <= K; k++) {
-            const nx = x + k * dx[i];
-            const ny = y + k * dy[i];
+  if (idx === alpha.length) {
+    return (dp[idx][x][y] = 1);
+  }
 
-            if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
-            if (findWord[depth] !== words[nx][ny]) continue;
+  dp[idx][x][y] = 0;
 
-            visited[x][y][depth] += dfs(depth + 1, nx, ny);
-        }
+  for (let i = 0; i < 4; i++) {
+    for (let k = 1; k <= K; k++) {
+      const nx = x + k * dx[i];
+      const ny = y + k * dy[i];
+
+      if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+      if (alpha[idx] !== map[nx][ny]) continue;
+
+      dp[idx][x][y] += dfs(nx, ny, idx + 1);
     }
+  }
 
-    return visited[x][y][depth];
+  return dp[idx][x][y];
 };
 
 let answer = 0;
 for (let i = 0; i < N; i++) {
-    for (let j = 0; j < M; j++) {
-        if (words[i][j] === findWord[0]) {
-            answer += dfs(1, i, j);
-        }
+  for (let j = 0; j < M; j++) {
+    if (map[i][j] === alpha[0]) {
+      answer += dfs(i, j, 1);
     }
+  }
 }
 
 console.log(answer);
