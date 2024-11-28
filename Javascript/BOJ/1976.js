bdb9271e68fa1b7ed4e6eebca5e616bf62b19249
suhwa <tnghk9611@naver.com>
2024-11-28 14:51:41 +0900

    BOJ_여행 가자

--- Javascript/BOJ/1976.js
+++ Javascript/BOJ/1976.js
@@ -1,60 +1,45 @@
-// 여행 가자
-
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-[N, M, ...info] = require("fs").readFileSync(filePath).toString().trim().split("\n");
-N = +N;
-M = +M;
-edge = info.splice(0, N).map((x) => x.split(" ").map(Number));
-city = info[0].split(" ").map(Number);
+const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
 
-// 인접 리스트 생성
-const graph = Array.from({ length: N + 1 }, () => []);
+const N = +input.shift();
+const M = +input.shift();
+const connect = input.slice(0, N).map((x) => x.split(" ").map(Number));
+
+const plan = input[N].split(" ").map(Number);
+
+const parent = Array.from({ length: N + 1 }, (_, idx) => idx);
+
+const find = (x) => {
+  if (parent[x] === x) return x;
+  return (parent[x] = find(parent[x]));
+};
+
+const union = (a, b) => {
+  const pa = find(a);
+  const pb = find(b);
+
+  if (pa < pb) parent[pb] = pa;
+  else parent[pa] = pb;
+};
+
 for (let i = 0; i < N; i++) {
-  for (let j = i + 1; j < N; j++) {
-    if (edge[i][j]) {
-      graph[i + 1].push(j + 1);
-      graph[j + 1].push(i + 1);
+  for (let j = 0; j < N; j++) {
+    if (connect[i][j]) {
+      union(i + 1, j + 1);
     }
   }
 }
 
-// dfs
-function dfs(start, goal) {
-  const stack = [start];
-  const visited = Array.from({ length: N + 1 }, () => 0);
-  visited[start] = 1;
+let answer = true;
 
-  while (stack.length) {
-    curNode = stack.pop();
+for (let i = 0; i < M - 1; i++) {
+  const a = plan[i];
+  const b = plan[i + 1];
 
-    // 목표물 찾으면 0 반환
-    if (curNode === goal) return 0;
-
-    for (let i = 0; i < graph[curNode].length; i++) {
-      adjNode = graph[curNode][i]; // 인접 노드
-
-      if (visited[adjNode]) continue;
-      visited[adjNode] = 1;
-      stack.push(adjNode);
-    }
+  if (find(a) !== find(b)) {
+    answer = false;
+    break;
   }
-
-  // 목표물 못찾으면 -1 반환
-  return -1;
 }
 
-function solution() {
-  let answer;
-  let isPossible = true;
-  // 0번 - M-1번 도시에 대해서 탐색
-  for (let i = 0; i < M - 1; i++) {
-    if (dfs(city[i], city[i + 1]) === -1) {
-      isPossible = false;
-      break;
-    }
-  }
-  answer = isPossible ? "YES" : "NO";
-  return answer;
-}
-
-console.log(solution());
+console.log(answer ? "YES" : "NO");
