5b859e434970124fa2124d8a0c6de6ae0a949b6d
suhwa <tnghk9611@naver.com>
2024-12-02 12:49:44 +0900

    BOJ_케이크 자르기

--- Javascript/BOJ/17179.js
+++ Javascript/BOJ/17179.js
@@ -1,51 +1,50 @@
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
 
-const [N, M, L] = input.shift().split(" ").map(Number);
-const S = input.splice(0, M).map(Number);
-S.push(L);
+const [N, M, L] = input[0].split(" ").map(Number);
+const cut = input.slice(1, M + 1).map(Number);
+const count = input.slice(M + 1).map(Number);
 
-const Q = input.map(Number);
+cut.push(L);
 
-const possible = (len, cnt) => {
-    let std = 0;
-    let c = 0;
-    for (let i = 0; i <= M; i++) {
-        if (S[i] - std >= len) {
-            c++;
-            std = S[i];
-        }
+const possible = (mid, count) => {
+  let cnt = 0;
+  let cur = 0;
 
-        if (c === cnt + 1) return true;
+  for (let i = 0; i <= M; i++) {
+    if (cut[i] - cur >= mid) {
+      cur = cut[i];
+      cnt++;
     }
+  }
 
-    return false;
+  if (cnt - 1 >= count) return true;
+  return false;
 };
 
-const binarySearch = (cnt) => {
-    let left = 2;
-    let right = L;
+const lb = (count) => {
+  let left = 2;
+  let right = L;
+  let max = 1;
 
-    let maxVal = 1;
+  while (left <= right) {
+    const mid = parseInt((left + right) / 2);
 
-    while (left <= right) {
-        const mid = parseInt((left + right) / 2);
-
-        if (possible(mid, cnt)) {
-            left = mid + 1;
-            maxVal = Math.max(maxVal, mid);
-        } else {
-            right = mid - 1;
-        }
+    if (possible(mid, count)) {
+      left = mid + 1;
+      max = Math.max(max, mid);
+    } else {
+      right = mid - 1;
     }
+  }
 
-    return maxVal;
+  return max;
 };
 
 const answer = [];
 
-for (let q of Q) {
-    answer.push(binarySearch(q));
+for (let c of count) {
+  answer.push(lb(c));
 }
 
 console.log(answer.join("\n"));
