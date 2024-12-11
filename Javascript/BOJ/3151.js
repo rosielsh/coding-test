f611ed021b6553cd424bdbd8d562165f32df8310
suhwa <tnghk9611@naver.com>
2024-12-11 13:45:51 +0900

    BOJ_합이 0

--- Javascript/BOJ/3151.js
+++ Javascript/BOJ/3151.js
@@ -1,48 +1,63 @@
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
 
-const N = +input.shift();
-const score = input
-    .shift()
-    .split(" ")
-    .map(Number)
-    .sort((a, b) => a - b);
+const N = +input[0];
+const arr = input[1]
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
 
 let answer = 0;
 
-for (let i = 0; i < N - 2; i++) {
-    let left = i + 1;
-    let right = N - 1;
+const lb = (idx, target) => {
+  let left = idx;
+  let right = N - 1;
 
-    while (left < right) {
-        const sum = score[i] + score[left] + score[right];
+  let minIdx = N;
 
-        if (sum === 0) {
-            if (score[left] === score[right]) {
-                answer += ((right - left + 1) * (right - left)) / 2;
-                break;
-            }
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
 
-            let lCnt = 1;
-            let rCnt = 1;
-
-            while (left + 1 < right && score[left] === score[left + 1]) {
-                lCnt++;
-                left++;
-            }
-
-            while (right - 1 > left && score[right] === score[right - 1]) {
-                rCnt++;
-                right--;
-            }
-
-            answer += lCnt * rCnt;
-
-            left++;
-            right--;
-        } else if (sum > 0) right--;
-        else if (sum < 0) left++;
+    if (arr[mid] >= target) {
+      right = mid - 1;
+      minIdx = Math.min(mid, minIdx);
+    } else {
+      left = mid + 1;
     }
+  }
+
+  return minIdx;
+};
+
+const ub = (idx, target) => {
+  let left = idx;
+  let right = N - 1;
+
+  let minIdx = N;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (arr[mid] > target) {
+      right = mid - 1;
+      minIdx = Math.min(mid, minIdx);
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return minIdx;
+};
+
+for (let i = 0; i < N - 1; i++) {
+  for (let j = i + 1; j < N; j++) {
+    // arr[i], arr[j] 2개의 수 선택
+    const t = -1 * (arr[i] + arr[j]);
+    const lbIdx = lb(j + 1, t);
+    const ubIdx = ub(j + 1, t);
+
+    answer += ubIdx - lbIdx;
+  }
 }
 
 console.log(answer);
