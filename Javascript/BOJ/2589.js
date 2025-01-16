d15bc21dbd0b38e28a0effbb8024df8e2e75ec82
suhwa <tnghk9611@naver.com>
2025-01-16 14:08:08 +0900

    BOJ_보물섬

--- Javascript/BOJ/2589.js
+++ Javascript/BOJ/2589.js
@@ -1,53 +1,46 @@
-// 보물섬
-
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let [RC, ...map] = require("fs").readFileSync(filePath).toString().trim().split("\n");
-const [r, c] = RC.split(" ").map(Number);
-map = map.map((x) => x.split(""));
+const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
+
+const [R, C] = input[0].split(" ").map(Number);
+const map = input.slice(1).map((x) => x.split(""));
 
 const dx = [0, 0, -1, 1];
 const dy = [-1, 1, 0, 0];
 
-function bfs(x, y) {
+const bfs = (x, y) => {
+  const visited = Array.from({ length: R }, () => Array(C).fill(false));
+  visited[x][y] = true;
+
+  let maxDepth = 0;
   const queue = [[x, y, 0]];
-  const visited = Array.from({ length: r }, () => Array(c).fill(0));
-  visited[x][y] = 1;
 
-  let maxDist = Number.MIN_SAFE_INTEGER;
+  while (queue.length > 0) {
+    const [cx, cy, depth] = queue.shift();
 
-  while (queue.length) {
-    const [cx, cy, dist] = queue.shift();
+    maxDepth = Math.max(depth, maxDepth);
 
     for (let i = 0; i < 4; i++) {
       const nx = cx + dx[i];
       const ny = cy + dy[i];
 
-      if (nx < 0 || nx >= r || ny < 0 || ny >= c || visited[nx][ny] || map[nx][ny] === "W")
-        continue;
+      if (nx < 0 || nx >= R || ny < 0 || ny >= C) continue;
+      if (visited[nx][ny] || map[nx][ny] === "W") continue;
 
-      visited[nx][ny] = 1;
-      maxDist = Math.max(maxDist, dist + 1);
-      queue.push([nx, ny, dist + 1]);
+      visited[nx][ny] = true;
+      queue.push([nx, ny, depth + 1]);
     }
   }
 
-  return maxDist;
-}
+  return maxDepth;
+};
 
-function solution() {
-  let answer;
-  let maxValue = Number.MIN_SAFE_INTEGER;
+let answer = 0;
 
-  for (let i = 0; i < r; i++) {
-    for (let j = 0; j < c; j++) {
-      if (map[i][j] === "L") {
-        maxValue = Math.max(maxValue, bfs(i, j));
-      }
-    }
+for (let i = 0; i < R; i++) {
+  for (let j = 0; j < C; j++) {
+    if (map[i][j] === "W") continue;
+    answer = Math.max(answer, bfs(i, j));
   }
-
-  answer = maxValue === Number.MIN_SAFE_INTEGER ? 0 : maxValue;
-  return answer;
 }
 
-console.log(solution());
+console.log(answer);
