14a73887fb7ef716b989abf240c7f77f508a9ea7
suhwa <tnghk9611@naver.com>
2025-01-22 13:20:08 +0900

    BOJ_수 나누기 게임

--- Javascript/BOJ/27172.js
+++ Javascript/BOJ/27172.js
@@ -1,38 +1,27 @@
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
 
-let N;
-let numbers;
-let isExist = Array.from({ length: 1000001 }, () => false);
-let score = Array.from({ length: 1000001 }, () => 0);
+const nums = input[1].split(" ").map(Number);
+const isExist = Array.from({ length: 1000001 }, () => false);
+const score = Array.from({ length: 1000001 }, () => 0);
 
-function solution() {
-  let answer = [];
-  N = +input[0];
-  numbers = input[1].split(" ").map(Number);
-
-  for (let i = 0; i < N; i++) {
-    isExist[numbers[i]] = true;
-  }
-
-  // 전체 카드 순회하면서
-  for (let i = 0; i < N; i++) {
-    const num = numbers[i];
-    // 배수들만 확인
-    for (let j = num * 2; j <= 1000000; j += num) {
-      // 만약에 배수가 존재하면 i가 승리, j는 패배
-      if (isExist[j]) {
-        score[num]++;
-        score[j]--;
-      }
-    }
-  }
-
-  for (let i = 0; i < N; i++) {
-    answer.push(score[numbers[i]]);
-  }
-
-  return answer.join(" ");
+for (let num of nums) {
+  isExist[num] = true;
 }
 
-console.log(solution());
+for (let num of nums) {
+  for (let i = num * 2; i <= 1000000; i += num) {
+    if (isExist[i]) {
+      score[num]++;
+      score[i]--;
+    }
+  }
+}
+
+const answer = [];
+
+for (let num of nums) {
+  answer.push(score[num]);
+}
+
+console.log(answer.join(" "));
